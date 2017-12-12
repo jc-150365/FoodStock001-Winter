0262113ecf16e5984d1fd7818831e4e78c1df502
謙太 可士村 <150365@jc-21.jp>
2017-12-12 15:29:37 +0900

    fix

--- FoodStock01/FoodStock01/FoodPageViewModel.cs
+++ FoodStock01/FoodStock01/FoodPageViewModel.cs
@@ -28,7 +28,7 @@ namespace FoodStock01
                     Magics = new ObservableCollection<Magic> {
                     new Magic {
                        F_name = food.F_name,
-                       F_Date = food.F_date
+                       F_Date = food.F_date.ToString()
                     }
                 };
                 }
@@ -49,7 +49,7 @@ namespace FoodStock01
     public class Magic
     {
         public string F_name { get; set; }
-        public DateTime F_Date { get; set; }
+        public string F_Date { get; set; }
     }
 
 }
